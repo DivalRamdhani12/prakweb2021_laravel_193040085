19fcf48115810563cc54344fe26edcd32559d5b2
Dival RF <divalrmd12@gmail.com>
2021-10-08 18:27:32 +0700

    Menyelesaikan Video ke 3 Laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>WPU BLOG | About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3><?= $name; ?></h3>
+    <h4><?= $email; ?></h4>
+    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200" height = "220">
+    
+</body>
+</html>
