0571d5c84c14a25407efc76eddd09ace32fef021
Dival RF <divalrmd12@gmail.com>
2021-10-09 11:25:54 +0700

    Menyelesaikan video ke 4 mengenai Materi Blade templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>WPU BLOG | About</title>
-</head>
-<body>
+ @extends('layouts.main')
+ 
+ @section('container')
     <h1>Halaman About</h1>
-    <h3><?= $name; ?></h3>
-    <h4><?= $email; ?></h4>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200" height = "220">
-    
-</body>
-</html>
+    <h3>{{ $name }}</h3>
+    <h4>{{ $email }}</h4>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" height = "220">
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home' , [
+        "title" => "Home"
+    ]);
 });
 
 
 Route::get('/about', function () {
     return view('about' , [
+        "title" => "About",
         "name" => "Dival Ramdhani Fadillah",
         "email" => "193040085.Dival@unpas.ac.id",
         "image" => "Dvl.jpeg"
@@ -27,5 +30,71 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Dival Ramdhani Fadillah",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
+            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
+            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus soluta laudantium, adipisci, culpa esse?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doni Santoso",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
+            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
+            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. 
+            Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, 
+            tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a 
+            rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus 
+             soluta laudantium, adipisci, culpa esse?"
+        ],
+        ];
+
+
+     return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//halaman singgle post
+
+Route::get('posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Dival Ramdhani Fadillah",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
+            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
+            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus soluta laudantium, adipisci, culpa esse?"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Doni Santoso",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
+            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
+            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. 
+            Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, 
+            tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a 
+            rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus 
+             soluta laudantium, adipisci, culpa esse?"
+        ],
+        ];
+
+        $new_post = [];
+        foreach($blog_posts as $post){
+            if($post["slug"] === $slug){
+                $new_post = $post;
+            }
+        }
+
+    return view ('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
