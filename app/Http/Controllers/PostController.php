2b8d83d4d529c8c7f41c62384cc87ee8b4db950b
Dival RF <divalrmd12@gmail.com>
2021-10-15 19:14:52 +0700

    Menyelesaikan video ke 7 mengenai Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,11 +13,11 @@ class PostController extends Controller
             "posts" => Post::all()
             ]);
     }
-    public function show($slug)
+    public function show(Post $post)
     {
         return view ('post', [
             "title" => "Single Post",
-            "post" => Post::find($slug)
+            "post" => $post
         ]);
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,13 @@
 
 namespace App\Models;
 
-class Post 
-{
-    private static $blog_posts = 
-    [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Dival Ramdhani Fadillah",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
-            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
-            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus soluta laudantium, adipisci, culpa esse?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Doni Santoso",
-            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos minus unde omnis 
-            facere necessitatibus eveniet at, non iste tempore aliquam veniam 
-            iure nam ducimus odio ea recusandae, inventore rem facilis quidem, maxime corrupti ullam. 
-            Vitae natus doloribus vero adipisci sapiente consectetur, enim ipsa! Sed hic fugiat incidunt, 
-            tempore itaque, qui earum animi impedit natus, odio est perferendis expedita ab eum dolorem a 
-            rerum odit deleniti sunt eligendi? Aut atque beatae aliquid sapiente error, delectus voluptatibus 
-             soluta laudantium, adipisci, culpa esse?"
-        ],
-        ];
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-    public static function find($slug)
-    {
-        $posts = static::all(); 
-        return $posts->firstwhere('slug', $slug);
-    }
+class Post extends Model
+{
+    use HasFactory;
+
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
