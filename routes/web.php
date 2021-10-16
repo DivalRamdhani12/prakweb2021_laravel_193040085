5ccd42a9f2f9453c1ca8f1e132dfe2d3ee3b1fc0
Dival RF <divalrmd12@gmail.com>
2021-10-16 15:29:07 +0700

    Menyelesaikan video ke 8 mengenai Post Ctaegory

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +31,21 @@ Route::get('/about', function () {
         "image" => "Dvl.jpeg"
     ]);
 });
-
 Route::get('/blog', [PostController::class, 'index']); 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);   
+
+Route::get('/categories', function(){
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+}); 
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+
+    ]);
+});
