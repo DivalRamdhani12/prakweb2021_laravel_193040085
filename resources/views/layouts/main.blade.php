317712bc746e5a86ef74a57f647f1bccf4b8a847
Dival RF <divalrmd12@gmail.com>
2021-11-01 23:15:56 +0700

    Menyelesaikan video ke 14 mengenai View Login dan Registration

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    
+    <!-- {{-- Bootstrap icons --}} -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+
+
+      <!-- {{-- My style --}} -->
+
+      <link rel="stylesheet" href="/css/style.css">
 
     <title>WPU BLOG | {{ $title }} </title>
   </head>
