8b2dc03a22d6ea6629e32f5fac634becf9aa82cc
Dival RF <divalrmd12@gmail.com>
2021-12-04 16:42:12 +0700

    Menyelesaikan Video ke 22 Mengenai Priview , Update & Delete Image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -104,6 +105,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max::255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
         if($request->slug != $post->slug) {
@@ -111,6 +113,14 @@ class DashboardPostController extends Controller
         }
 
         $validateData = $request->validate($rules);
+
+        if($request->file('image')){
+           if($request->oldImage){
+            Storage::delete($request->oldImage);
+           }
+            $validatedData['image']  = $request->file('image')->store('post-images');
+           }
+
 
 
         $validateData['user_id'] = auth()->user()->id;
@@ -130,6 +140,10 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image){
+            Storage::delete($post->image);
+        }
+
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted');
--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -42,9 +42,10 @@
 
       <div class="mb-3">
               <label for="image" class="form-label">Post Image</label>
-              <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image">
+              <img class="img-preview img-fluid mb-3 col-sm-5">
+              <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
               @error('image')
-             <div class="invalid-feedback">
+              <div class="invalid-feedback">
                 {{ $message }}
              </div>
              @enderror
@@ -76,5 +77,20 @@
     document.addEventListener('trix-file-accept', function(e){
       e.preventDefault();
     })
+
+        function previewImage()
+    {
+      const image = document.querySelector('#image');
+      const imgPreview = document.querySelector('.img-preview');
+      imgPreview.style.display = 'block';
+      const oFReader = new FileReader();
+      oFReader.readAsDataURL(image.files[0]);
+      oFReader.onload = function(oFREvent){
+        imgPreview.src = oFREvent.target.result;
+      }
+    }
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
 </script>
 @endsection
