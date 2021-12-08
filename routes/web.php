94d8e9c0ebd7192fdaa92d2cf4bfcbbcf1070863
Dival RF <divalrmd12@gmail.com>
2021-12-08 17:21:24 +0700

    Menyelesaikan video ke 23 mengenai Authorization

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -61,3 +62,4 @@ Route::get('/dashboard', function(){
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSLug'])->middleware('auth');
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
